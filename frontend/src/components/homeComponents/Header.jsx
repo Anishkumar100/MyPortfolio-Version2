7ce483005d4aabf439b91523a6f3398e3249ca5e
AnishKumar100 <akcoder1102004@gmail.com>
2025-08-17 20:50:12 +0530

    new changes

--- frontend/src/components/homeComponents/Header.jsx
+++ frontend/src/components/homeComponents/Header.jsx
@@ -29,7 +29,7 @@ export const Header = () => {
         className=" 
     relative
     w-full max-w-4xl
-    mx-auto top-28 mb-10  max-sm:top-28 flex flex-col items-center justify-center
+    mx-auto top-28 mb-10  max-sm:top-20 flex flex-col items-center justify-center
     px-4 sm:px-6 md:px-8 lg:px-12
     text-center text-white 
   
